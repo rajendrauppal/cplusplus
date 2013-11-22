e1aa865dfd96f5a4401a801e73c192b232284548
Rajendra Kumar Uppal <rajen.iitd@gmail.com>
2013-11-22 08:20:38 +0530

    Written skeleton of Binary Search program

--- src/BinarySearch.cpp
+++ src/BinarySearch.cpp
@@ -6,12 +6,26 @@
 
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
 int BinarySearch()
 {
 	return 0;
 }
 
-int main()
+void usage()
 {
+	cout << "Usage:" << endl;
+	cout << "BinarySearch.exe <Array> <SearchItem>" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	if ( argc != 2 ) {
+		usage();
+		exit(1);
+	}
 	return 0;
 }
