32dac5a44f2251d4e13b731f61fa434fc1e10ae1
Rajendra Kumar Uppal <rajen.iitd@gmail.com>
2013-11-24 13:20:47 +0530

    Added Exceptions.h for common exceptions, Wrote IsFibonacci and Fibonacci series functions and their tests

--- src/Fibonacci.cpp
+++ src/Fibonacci.cpp
@@ -22,13 +22,81 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-bool Fibonacci(int n)
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::sort;
+
+#include "Exceptions.h"
+
+#define uint unsigned int
+#define LONGLONG long long
+
+/*
+* Finds nth Fibonacci number
+*/
+int Fibonacci(int n)
+{
+	return 0;
+}
+
+/*
+* Determines if a given number is Fibonacci or not
+*/
+bool IsFibonacci(int n)
 {
 	return true;
 }
 
+/*
+* Generated Fibonacci series up to n
+*/
+vector<LONGLONG> FibonacciSeries(LONGLONG n)
+{
+	if ( n <= 0 ) throw InvalidInputException();
+	
+	vector<LONGLONG> result;
+
+	LONGLONG first = 1;
+	LONGLONG second = 1;
+	while ( n != 0 ) {
+		if ( n <= 2 ) {
+			for (LONGLONG i = 0; i < n; ++i) {
+				result.push_back(1);
+			}
+			break;
+		}
+		LONGLONG sum = first + second;
+		result.push_back(sum);
+		first = second;
+		second = sum;
+		n--;
+	}
+	sort(result.begin(), result.end());
+	return result;
+}
+
 int main()
 {
+	// Tests - generating Fibonacci series up to number n
+	int MAX_INT = 46; // maximum 46 Fibonacci numbers with int
+	LONGLONG MAX_LONGLONG = 92; // maximum 92 Fibonacci numbers with long long
+	try {
+		vector<LONGLONG> result = FibonacciSeries(MAX_LONGLONG);
+		for ( auto start = result.begin(); start != result.end(); ++start ) {
+			cout << *start << endl;
+		}
+		cout << endl;
+	}
+	catch (InvalidInputException e) {
+		cout << e.message() << endl;
+	}
+
+	cout << "Press Enter to continue..." << endl;
+	cin.get();
 	return 0;
 }
