2007d89e754f38c4fa6db56d6193b38c6ad7dacb
Rajendra Kumar Uppal <rajen.iitd@gmail.com>
2013-11-24 08:01:59 +0530

    Implementated recursive and iterative factorial finding functions

--- src/Factorial.cpp
+++ src/Factorial.cpp
@@ -23,12 +23,67 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <iostream>
 
-bool Factorial(int n)
+#include "Factorial.h"
+
+#define LLONG long long
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+/*
+* Iterative implementation of factorial finding function
+*/
+LLONG Factorial_Iterative(LLONG n)
 {
-	return true;
+	if ( n < 0 ) throw InvalidInputException();
+	if ( n <= 1 ) return 1;
+	LLONG f = 1;
+	while ( n != 1 ) {
+		f *= n;
+		n -= 1;
+	}
+	return f;
+}
+
+/*
+* Recursive implementation of factorial finding function
+*/
+LLONG Factorial_Recursive(LLONG n)
+{
+	if ( n < 0 ) throw InvalidInputException();
+	if ( n <= 1 ) return 1;
+	else {
+		return n * Factorial_Recursive( n - 1 );
+	}
 }
 
 int main()
 {
+	int numbers[6] = {-1, 0, 5, 10, 47, 50};
+	// Iterative factorial function tests
+	for ( unsigned int i = 0; i < 6; ++i ) {
+		try {
+			cout << Factorial_Iterative(numbers[i]) << endl;
+		} 
+		catch (InvalidInputException e) {
+			cout << e.message() << endl;
+		}
+	}
+	cout << endl;
+
+	// Recursive factorial function tests
+	for ( unsigned int i = 0; i < 6; ++i ) {
+		try {
+			cout << Factorial_Recursive(numbers[i]) << endl;
+		}
+		catch (InvalidInputException e) {
+			cout << e.message() << endl;
+		}
+	}
+	cout << endl;
+
+	cout << "Please press Enter to continue..." << endl;
+	cin.get();
 	return 0;
 }
