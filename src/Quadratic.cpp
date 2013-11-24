061e3584e6af4c95ae82251015393eb68857f569
Rajendra Kumar Uppal <rajen.iitd@gmail.com>
2013-11-24 21:05:56 +0530

    Implemented Quadratic equation solver

--- src/Quadratic.cpp
+++ src/Quadratic.cpp
@@ -22,13 +22,47 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include <iostream>
+#include <math.h>
 
-bool Quadratic(int n)
+using std::cout;
+using std::cin;
+using std::endl;
+using std::sqrt;
+using std::pow;
+
+struct Roots
 {
-	return true;
+	double alpha;
+	double beta;
+};
+
+/*
+* Solves a quadratic equation of the form
+* (a * x ^ 2 + b * x + c) = 0
+* alpha = -b + sqrt(b ^ 2 - 4 * a * c) / 2 * a
+* beta = -b - sqrt(b ^ 2 - 4 * a * c) / 2 * a
+*/
+Roots Quadratic(double a, double b, double c)
+{
+	Roots roots;
+	double d = sqrt(pow(b, 2) - 4 * a * c);
+	roots.alpha = ( -b + d ) / ( 2 * a );
+	roots.beta = ( -b - d ) / ( 2 * a );
+	return roots;
 }
 
 int main()
 {
+	// Tests - Solving quadratic equations
+	Roots roots = Quadratic(3, 2, -1);	// 3x^2 + 2*x - 1 = 0
+	cout << "3x^2 + 2*x - 1 = 0" << endl;
+	cout << "alpha = " << roots.alpha << " beta = " << roots.beta << endl;
+
+	roots = Quadratic(1, 2, 1);	// x^2 + 2*x + 1 = 0
+	cout << "x^2 + 2*x + 1 = 0" << endl;
+	cout << "alpha = " << roots.alpha << " beta = " << roots.beta << endl;
+
+	cout << "Press Enter to continue..." << endl;
+	cin.get();
 	return 0;
 }
